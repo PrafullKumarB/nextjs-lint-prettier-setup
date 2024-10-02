1f6a4c75140654e3edda25e013bea85506103822
u1184313 <Prafullkumar@novastrid.com>
2024-10-03 04:20:59 +0530

    Linting testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,16 @@ export default function Home() {
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
           width={180}
+
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  <ol>
+    <li>
+      Get started by editing <code>src/app/page.tsx</code>.
+    </li>
+    <li>Save and see your changes instantly.</li>
+  </ol>
 
         <div className={styles.ctas}>
           <a

3b0ab444f2c4bf230fa2ec7cb2d6ba1f63d5eb34
u1184313 <Prafullkumar@novastrid.com>
2024-10-03 04:37:32 +0530

    Linting testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,43 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "next/core-web-vitals",
-    "plugin:react/recommended",
-    "airbnb",
-    "prettier",
-  ],
-  overrides: [
-    {
-      files: ["**.ts", "**.tsx"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    "react/jsx-props-no-spreading": "off",
-    "react/react-in-jsx-scope": "off",
-    "eol-last": 1,
-    // "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
-    "no-multiple-empty-lines" :"error",
-    "react/jsx-filename-extension": [
-      1,
-      {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-      },
-    ]
-  },
-  settings: {
-    react: {
-      version: "18.2.0",
+    extends: [
+        "next/core-web-vitals",
+        "plugin:react/recommended",
+        "airbnb",
+        "prettier",
+    ],
+    overrides: [
+        {
+            files: ["**.ts", "**.tsx"],
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-  },
+    rules: {
+        "react/jsx-props-no-spreading": "off",
+        "react/react-in-jsx-scope": "off",
+        "eol-last": 1,
+        // "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+        "no-multiple-empty-lines": "error",
+        "react/jsx-filename-extension": [
+            1,
+            {
+                extensions: [".ts", ".tsx", ".js", ".jsx"],
+            },
+        ],
+    },
+    settings: {
+        react: {
+            version: "18.2.0",
+        },
+    },
 };
